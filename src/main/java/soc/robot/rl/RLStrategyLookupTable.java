2a19562911f4700501d8cb9887dcc792fc194f16
Admin <Admin@DESKTOP-14T3936>
2018-11-03 11:59:15 +0100

    RLStrategyLookupTable created

---
+++ src/main/java/soc/robot/rl/RLStrategyLookupTable.java
@@ -0,0 +1,164 @@
+package soc.robot.rl;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Random;
+
+import soc.game.SOCPlayer;
+import soc.robot.SOCRobotBrain;
+
+
+
+public class RLStrategyLookupTable extends RLStrategy{
+
+	/** variable to remember previous state of type 1: for all opponents */
+    protected HashMap<SOCPlayer, int[]> oldState;
+    
+    /** variable to remember previous general state (type 2) */
+    protected int[] oldState2;
+    
+    /** Memory for all the states type 1*/
+    protected HashMap<int[], Double> states;
+    
+    /** Memory for all the states type 2 */
+    protected HashMap<int[], Double> states2;
+	
+	public RLStrategyLookupTable(SOCRobotBrain br) {
+		super(br);
+		// TODO Auto-generated constructor stub
+		state = new SOCState(ourPlayerNumber, playerTrackers);
+        state.updateAll(playerTrackers, board);   
+        
+        
+        //TO DO: read these from file - make a function: read memory
+        states = new HashMap<int[], Double>();    
+        states2 = new HashMap<int[], Double>(); 
+        oldState = new HashMap<SOCPlayer, int[]>();
+        oldState2 = new int[6];
+        
+        ArrayList<CustomPair> opp_states = new ArrayList<CustomPair>();
+
+        /* adding to memory the state at the beginning of the game */
+    	for (SOCPlayer opp : opponents) {
+    		int[] playerState = state.getState(opp);
+    		int points = opp.getPublicVP();
+    		Double value = Double.valueOf(0.5); //or maybe random?
+			states.put(playerState, value);
+			oldState.put(opp, playerState);
+    		
+    		int state_value = Math.round(value.floatValue()*10);	
+    		
+    		opp_states.add(new CustomPair(Integer.valueOf(points), Integer.valueOf(state_value)));
+    		
+    	}
+    	
+    	int[] secondState = new int[6];
+    	
+    	for(int i = 0; i<opp_states.size(); i++) {
+    		secondState[i*2] = opp_states.get(i).getKey().intValue();
+    		secondState[i*2 + 1] = opp_states.get(i).getValue().intValue();
+    	}
+    	
+    	Double value = Double.valueOf(0.5); //or maybe random?
+		states2.put(secondState, Double.valueOf(value));
+		oldState2 = secondState;
+		
+	}
+	
+	
+	protected float getStateValue(SOCState tmpState) {
+    	ArrayList<CustomPair> opp_states = new ArrayList<CustomPair>();
+
+    	for (SOCPlayer opp : opponents) {
+    		int[] playerState = tmpState.getState(opp);
+    		int points = opp.getPublicVP();
+    		Double value = states.get(playerState);
+    		if (value==null) {
+    			value = new Random().nextGaussian()*0.05 + 0.5;
+    			states.put(playerState, Double.valueOf(value));
+    		}
+    		
+    		int state_value = Math.round(value.floatValue()*10);	
+    		
+    		opp_states.add(new CustomPair(Integer.valueOf(points), Integer.valueOf(state_value)));
+    		
+    	}
+    	
+    	int[] secondState = new int[6];
+    	opp_states.sort(new Comparator<CustomPair>() {
+		    public int compare(CustomPair o1, CustomPair o2) {
+		        return o2.getKey().compareTo(o1.getKey());
+		    }
+		});
+    	
+    	for(int i = 0; i<opp_states.size(); i++) {
+    		secondState[i*2] = opp_states.get(i).getKey().intValue();
+    		secondState[i*2 + 1] = opp_states.get(i).getValue().intValue();
+    	}
+    	
+    	Double value = states2.get(secondState);
+		if (value!=null) {}
+		else {
+			value = new Random().nextGaussian()*0.05 + 0.5; //or maybe random?
+			states2.put(secondState, Double.valueOf(value));
+		}
+		
+		return value.floatValue();
+
+    }
+	
+	 protected void updateStateValue() {
+	    	state.updateAll(playerTrackers, board);
+	    	ArrayList<CustomPair> opp_states = new ArrayList<CustomPair>();
+	    	
+	    	for (SOCPlayer opp : opponents) {
+	    		int[] oldPlayerState = oldState.get(opp);
+	    		Double oldPlayerStateValue = states.get(oldPlayerState);
+	    		
+	    		int[] newPlayerState = state.getState(opp);
+	    		Double newPlayerStateValue = states.get(newPlayerState);
+	    		
+	    		if (newPlayerStateValue==null) {
+	    			newPlayerStateValue = new Random().nextGaussian()*0.05 + 0.5; //or maybe random?
+	    			states.put(newPlayerState, Double.valueOf(newPlayerStateValue));
+	    		}
+	    		
+	    		oldPlayerStateValue = oldPlayerStateValue + alpha * (gamma * newPlayerStateValue - oldPlayerStateValue);
+	    		
+	    		states.put(oldPlayerState, Double.valueOf(oldPlayerStateValue));
+	    		oldState.put(opp, newPlayerState);
+	    		
+	    		//calculation to get new state array
+	    		int points = opp.getPublicVP();
+	    		int roundedPlayerStateValue = Math.round(newPlayerStateValue.floatValue()*10);
+	    		opp_states.add(new CustomPair(Integer.valueOf(points), Integer.valueOf(roundedPlayerStateValue)));
+	    	}
+	    	
+	    	int[] newState = new int[6];
+	    	opp_states.sort(new Comparator<CustomPair>() {
+			    public int compare(CustomPair o1, CustomPair o2) {
+			        return o2.getKey().compareTo(o1.getKey());
+			    }
+			});
+	    	for(int i = 0; i<opp_states.size(); i++) {
+	    		newState[i*2] = opp_states.get(i).getKey().intValue();
+	    		newState[i*2 + 1] = opp_states.get(i).getValue().intValue();
+	    	}
+	    	Double newStateValue = states2.get(newState);
+			if (newStateValue==null) {
+//				newStateValue = Double.valueOf(0.5); //or maybe random?
+				newStateValue = new Random().nextGaussian()*0.05 + 0.5;
+				states2.put(newState, Double.valueOf(newStateValue));
+			}
+	    	
+			Double oldStateValue = states2.get(oldState2);
+	    	
+	    	oldStateValue = oldStateValue + alpha * (gamma * newStateValue  - oldStateValue);
+	    	
+	    	states2.put(oldState2, Double.valueOf(oldStateValue));
+	    	oldState2 = newState;	    	
+	    	currentStateValue = newStateValue;
+	    }
+
+}
